70bdfdf768b90f7af13aa7867239660f8b7214f9
Nick Mathewson <nickm@torproject.org>
2012-04-06 11:05:35 -0400

    Replace more deferred_cb names with event_callback

--- bufferevent-internal.h
+++ bufferevent-internal.h
@@ -179,7 +179,7 @@ struct bufferevent_private {
 	int dns_error;
 
 	/** Used to implement deferred callbacks */
-	struct deferred_cb deferred;
+	struct event_callback deferred;
 
 	/** The options this bufferevent was constructed with */
 	enum bufferevent_options options;
